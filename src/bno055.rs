83be72683baf5615334dd10f8a3a755ac61b0e53
TheZoq2 <frans.skarman@gmail.com>
2017-05-21 18:04:15 +0200

    Add missing file

---
+++ src/bno055.rs
@@ -0,0 +1,134 @@
+extern crate nalgebra as na;
+
+use std::thread;
+use std::time::Duration;
+
+use i2cdev::core::I2CDevice;
+use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+
+const MODE_CONFIG: u8 = 0x00;
+const MODE_NDOF: u8 = 0x0C;
+
+const OPR_MODE_REG: u8 = 0x3D;
+
+const POWER_MODE_NORMAL: u8 = 0x0;
+const POWER_MODE_REG: u8 = 0x03E;
+
+const GRAVITY_VECTOR_REG: u8 = 0x2E;
+
+//Selects internal or external clock?
+const SYS_TRIGGER_ADDR: u8 = 0x3f;
+const SYSTEM_STATUS_ADDR: u8 = 0x39;
+const SYSTEM_ERROR_ADDR: u8 = 0x3a;
+
+const PAGE_ID_ADDR: u8 = 0x07;
+
+use i2c_helpers;
+
+#[derive(Debug)]
+pub struct Bno055Status
+{
+    pub status: u8,
+    pub error: u8
+}
+
+pub struct Bno055
+{
+    i2c_device: LinuxI2CDevice
+}
+
+impl Bno055
+{
+    /*
+        pub fn new(mut i2c_device: LinuxI2CDevice) -> Result<Bno055, LinuxI2CError>
+        {
+            //Send some dummy data
+            i2c_device.smbus_write_byte_data(PAGE_ID_ADDR, 0);
+
+            //Enter config mode
+            i2c_device.smbus_write_byte_data(OPR_MODE_REG, MODE_CONFIG)?;
+            //Wait for the mode to change
+            thread::sleep(Duration::from_millis(20));
+
+            i2c_device.smbus_write_byte_data(PAGE_ID_ADDR, 0);
+
+            //Reset the chip
+            i2c_device.smbus_write_byte_data(SYS_TRIGGER_ADDR, 0x20);
+            //Sleep until the chip is ready
+            thread::sleep(Duration::from_millis(650));
+
+            //Set power mode
+            i2c_device.smbus_write_byte_data(POWER_MODE_REG, POWER_MODE_NORMAL)?;
+            //use internal oscilator
+            i2c_device.smbus_write_byte_data(SYS_TRIGGER_ADDR, 0x0)?;
+
+            //Set operation mode
+            i2c_device.smbus_write_byte_data(OPR_MODE_REG, MODE_NDOF)?;
+
+            //Wait for the mode to change
+            thread::sleep(Duration::from_millis(20));
+
+            Ok (Bno055 {
+                i2c_device
+            })
+        }
+    */
+
+    pub fn new(mut i2c_device: LinuxI2CDevice) -> Result<Bno055, LinuxI2CError>
+    {
+        /*
+        //Send a reset command
+        i2c_device.smbus_write_byte_data(SYS_TRIGGER_ADDR, 0x20);
+        thread::sleep(Duration::from_millis(1000));
+
+        //Set config omde
+        i2c_device.smbus_write_byte_data(OPR_MODE_REG, MODE_CONFIG);
+
+        i2c_device.smbus_write_byte_data(POWER_MODE_REG, POWER_MODE_NORMAL);
+
+        //Set the correct page, not sure what this does
+        i2c_device.smbus_write_byte_data(PAGE_ID_ADDR, 0);
+        //Set internal clock
+        i2c_device.smbus_write_byte_data(SYS_TRIGGER_ADDR, 0);
+
+        i2c_device.smbus_write_byte_data(OPR_MODE_REG, MODE_NDOF);
+        thread::sleep(Duration::from_millis(20));
+        */
+
+        Ok (Bno055 {
+            i2c_device
+        })
+    }
+
+    pub fn get_chip_id(&mut self) -> Result<u8, LinuxI2CError>
+    {
+        self.i2c_device.smbus_read_byte_data(0x00)
+    }
+
+    pub fn get_gravity_vector(&mut self) -> Result<na::Vector3<i16>, LinuxI2CError>
+    {
+        self.read_vector3(GRAVITY_VECTOR_REG)
+    }
+
+    pub fn get_system_status(&mut self) -> Result<Bno055Status, LinuxI2CError>
+    {
+        Ok (
+            Bno055Status
+            {
+                status: self.i2c_device.smbus_read_byte_data(SYSTEM_STATUS_ADDR)?,
+                error: self.i2c_device.smbus_read_byte_data(SYSTEM_ERROR_ADDR)?,
+            }
+        )
+    }
+
+    pub fn read_vector3(&mut self, start_addr: u8) -> Result<na::Vector3<i16>, LinuxI2CError>
+    {
+        Ok(
+            na::Vector3::new(
+                i2c_helpers::read_i16_i2c_big_endian(&mut self.i2c_device, start_addr)?,
+                i2c_helpers::read_i16_i2c_big_endian(&mut self.i2c_device, start_addr + 2)?,
+                i2c_helpers::read_i16_i2c_big_endian(&mut self.i2c_device, start_addr + 4)?
+            )
+        )
+    }
+}
